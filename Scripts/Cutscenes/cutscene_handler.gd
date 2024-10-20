99a2ae8b10afa2f462878dbdc90b8941fb1c478a
McKinley <caiohudson.red4@hotmail.com>
2024-10-20 11:28:44 -0300

    GameIntro implemented
    - Need to add special effects now

--- Scripts/Cutscenes/cutscene_handler.gd
+++ Scripts/Cutscenes/cutscene_handler.gd
@@ -2,9 +2,17 @@ extends Node2D
 
 @export var cutscene_player: AnimationPlayer
 @export var skipButton : Button
+@export var cutsceneName : String
 
 func _ready() -> void:
-	cutscene_player.play("intro_cutscene/cutscene_1") # Starts the Cutscene
+	cutscene_player.play(cutsceneName) # Starts the Cutscene
+
+func _process(delta: float) -> void:
+	if(Input.is_anything_pressed() and not cutscene_player.is_playing()):
+		cutscene_player.play()
+	
+func _pause() -> void:
+	cutscene_player.pause()
 	
 func _skip() -> void:
 	# Skip scene
