12c7d6e4e885c29d001fa92be477be44e52ad37f
Raniery Almeida <ranie-ry@outlook.com>
2024-10-19 22:11:11 -0300

    fix: flags de mudança de estado

--- Scripts/player_char.gd
+++ Scripts/player_char.gd
@@ -13,6 +13,7 @@ const state_type = preload("res://Scripts/state_type_enum.gd").state_type
 var state_queue:StateQueue
 var default_state: CharacterState
 var current_state:CharacterState
+var previous_state: CharacterState
 
 func _ready():
 	state_queue = StateQueue.new(max_state_queue_size)
@@ -27,7 +28,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction and current_state.type != state_type.attack:
 		velocity.x = direction * speed
 		if direction < 0 && velocity.x < 0:
 			_sprite2d.flip_h = true
@@ -54,20 +55,21 @@ func _physics_process(delta):
 func _process(_delta):
 	update_state()
 	
-	if Input.is_action_just_pressed("basic_attack") and is_on_floor():
+	if Input.is_action_just_pressed("basic_attack") and is_on_floor() and current_state.type != state_type.attack:
 		end_state()
 		enqueue_state(state_type.attack)
 	
-	if current_state.type == state_type.idle:
+	if state_just_entered(state_type.idle):
 		_animation_player.play("idle")
-	if current_state.type == state_type.walk:
+	if state_just_entered(state_type.walk):
 		_animation_player.play("walk")
-	if current_state.type == state_type.jump:
+	if state_just_entered(state_type.jump):
 		_animation_player.play("jump")
-	if current_state.type == state_type.attack:
+	if state_just_entered(state_type.attack):
 		_animation_player.play("attack")
 
 func update_state():
+	previous_state = current_state
 	if current_state.done and not is_idle():
 		current_state = state_queue.dequeue()
 	
@@ -88,6 +90,9 @@ func is_idle():
 		return true
 
 	return false
+
+func state_just_entered(type:state_type):
+	return current_state.type == type && previous_state.type != type
 	
 func _on_animation_player_animation_finished(anim_name:StringName):
 	if anim_name == "attack":
