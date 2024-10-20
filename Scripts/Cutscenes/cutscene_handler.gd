d341a0026e7db1bcd80c5c5ef56f92c4016183d0
McKinley <caiohudson.red4@hotmail.com>
2024-10-20 15:09:26 -0300

    Cutscene inicial com audios de Menu e Intro feitos

--- Scripts/Cutscenes/cutscene_handler.gd
+++ Scripts/Cutscenes/cutscene_handler.gd
@@ -1,24 +1,28 @@
 extends Node2D
 
 @export var cutscene_player: AnimationPlayer
+
+@export var nextButton : Button
 @export var skipButton : Button
+
 @export var dialogueLabel : Label
 
 @export var cutsceneName : String
 
 func _ready() -> void:
 	cutscene_player.play(cutsceneName) # Starts the Cutscene
-	skipButton.visible = false
+	nextButton.visible = false
 
 func _process(delta: float) -> void:
 	if(Input.is_anything_pressed() and not cutscene_player.is_playing()):
 		cutscene_player.play()
 		
-	if(Input.is_anything_pressed()):
-		skipButton.visible = true
+		
 	
 func _pause() -> void:
 	cutscene_player.pause()
+	nextButton.visible = true
+	
 	
 func _skip() -> void:
 	# Skip scene
@@ -27,3 +31,9 @@ func _skip() -> void:
 
 func _on_skip_button_pressed() -> void:
 	_skip()
+
+
+func _on_next_button_pressed() -> void:
+	if(not cutscene_player.is_playing()):
+		cutscene_player.play()
+		nextButton.visible = false
