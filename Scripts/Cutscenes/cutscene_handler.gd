b0d33a895fab4ea6ae153516a6fb59a5ba3ef6a6
McKinley <caiohudson.red4@hotmail.com>
2024-10-20 00:24:07 -0300

    Added Cutscene structure

---
+++ Scripts/Cutscenes/cutscene_handler.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var cutscene_player: AnimationPlayer
+@export var skipButton : Button
+
+func _ready() -> void:
+	cutscene_player.play("intro_cutscene/cutscene_1")
+func _skip() -> void:
+	# Skip scene
+	cutscene_player.seek(cutscene_player.current_animation_length,true);
+
+
+func _on_skip_button_pressed() -> void:
+	_skip()
