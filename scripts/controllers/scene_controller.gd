30328b4805c0070400e2cb620dd4b7c6ee6a9edb
Wiltzy <wiltzy.draws@gmail.com>
2024-11-30 21:27:43 -0300

    updated scene names

--- scripts/controllers/scene_controller.gd
+++ scripts/controllers/scene_controller.gd
@@ -15,7 +15,7 @@ func _ready() -> void:
 
 	# Load first scene
 	# load_scene("res://scenes/screens/menu_interface.tscn")
-	load_scene("res://scenes/screens/levels/level_circus_1.tscn")
+	load_scene("res://scenes/screens/levels/lvl_circus_1.tscn")
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
