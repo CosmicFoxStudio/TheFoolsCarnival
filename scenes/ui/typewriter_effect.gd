5de7dfc984baad9ef920ac4204767606fb4f813d
McKinley <caiohudson.red4@hotmail.com>
2025-01-13 22:32:49 -0300

    Settings are now being toogled

--- scenes/ui/typewriter_effect.gd
+++ scenes/ui/typewriter_effect.gd
@@ -1,13 +1,15 @@
 class_name TypewriterEffect extends Label
 
 
-func typewriter_effect(duration : float = 2) -> void:
+func typewriter_effect(duration : float = 2, speed : float = 2.5) -> void:
 	var timeElapsed = 0
-	while timeElapsed < duration:
+	while timeElapsed <= duration:
 		visible_ratio = lerp(0,1, timeElapsed / duration)
 		timeElapsed += get_process_delta_time()
-		await get_tree().create_timer(get_process_delta_time() * 2.5).timeout
-		print(visible_ratio)
+		await get_tree().create_timer(get_process_delta_time() * speed).timeout
+	
+	visible_ratio = 1
+		
 		
 		
 		
